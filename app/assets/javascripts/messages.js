45eb318c38feb19e38d70e35430111a0f865bf99
Harley Trung <harley@bsa.edu.vn>
2016-10-31 21:01:14 +0700

    remove coffee

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,10 @@
+window.updateMessages = function() {
+  $.getJSON(window.location.pathname + ".json", function(messages) {
+    e = $('.messages')
+    e.html('')
+    for (i in messages) {
+      message = messages[i]
+      e.append("<div class='message'><strong>" + message.username + "</strong> " + message.content + "</div>")
+    }
+  })
+}
