3d25e69dab453e306b873c826185d753139c3d6e
Trung Nguyen <ndt8895@gmail.com>
2016-11-03 18:48:58 +0700

    Milestone 6

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -18,3 +18,30 @@ window.updateMessagesViaJSON = function() {
 window.updateMessagesViaJS = function(){
   $.ajax({url: window.location.path, dataType:'script'});
 }
+
+
+console.log("setting up messages channel", App.cable);
+
+App.messages = App.cable.subscriptions.create("MessagesChannel", {
+  connected: function() {
+    console.log("connected");
+    // Called when the subscription is ready for use on the server
+  },
+  disconnected: function() {
+    console.log("disconnected");
+    // Called when the subscription has been terminated by the server
+  },
+  received: function(data) {
+    console.log({
+      "data received": data
+    });
+    // Called when there's incoming data on the websocket for this channel
+    $(".messages").append(data.message);
+  },
+  test: function(data) {
+    // Notice that this calls the javascript defined by MessagesChannel#test on the server
+    this.perform('test', {
+      message: data
+    });
+  }
+});
